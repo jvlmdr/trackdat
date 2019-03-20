ef87c3960c6c9414a53f6848877685127fe10159
Jack Valmadre <jack.valmadre@gmail.com>
2019-03-20 23:27:31 +0000

    Support frame subset in unpack_tracknet

--- scripts/unpack_tracknet.sh
+++ scripts/unpack_tracknet.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Use environment variable to set subsample rate.
+TRACKNET_RATE="${TRACKNET_RATE:-1}"
+
+if [ "${TRACKNET_RATE}" -gt 1 ]; then
+    default_name="tracknet_${TRACKNET_RATE}"
+else
+    default_name=tracknet
+fi
+
 dl="${1:-./dl/tracknet}"
-data="${2:-./data/tracknet}"
+data="${2:-"./data/${default_name}"}"
 
 mkdir -p "${data}"
 dl="$( cd "${dl}" && pwd )"
@@ -15,5 +24,25 @@ rsync -av "${dl}/data/" "${data}/" || exit 1
     cd "${dl}/TrackingNet-devkit" || exit 1
     python extract_frame.py --trackingnet_dir "${data}" || exit 1
 ) || exit 1
-
+# Then remove zips.
 rm -r "${data}"/*/zips || exit 1
+
+# Take subset of frames to reduce size.
+if [ "${TRACKNET_RATE}" -gt 1 ]; then
+    chunks="TEST $( seq 0 11 | xargs -I{} echo TRAIN_{} )"
+    for chunk in $chunks; do
+        (
+            cd "$data/$chunk" && \
+            find frames -type f -name '*.jpg' \
+                | sed -e 's/\.jpg$//' \
+                | awk -F/ '($3 % '"${TRACKNET_RATE}"' == 0) {print $2 "/" $3 ".jpg"}' \
+                >subset.txt && \
+            rm -rf frames_tmp && \
+            mkdir frames_tmp && \
+            rsync --files-from=subset.txt frames/ frames_tmp/ && \
+            rm subset.txt && \
+            rm -r frames && \
+            mv frames_tmp frames
+        ) || exit 1
+    done
+fi
